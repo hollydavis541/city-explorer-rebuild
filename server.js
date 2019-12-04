16127ad45140098f0a5c0c7536ebae1fb9baea2e
Holly Davis <hollydavis541@gmail.com>
2019-12-03 22:05:54 -0800

    created Location constructor and route

--- server.js
+++ server.js
@@ -14,4 +14,19 @@ app.use(cors());
 const client = new pg.Client(process.env.DATABASE_URL);
 client.on('err', err => console.error(err));
 
+// Constructors
+function Location(query, city){
+  this.search_query = query;
+  this.address = city.formatted_address;
+  this.latitude = city.geometry.location.lat;
+  this.longitude = city.geometry.location.lng;
+  this.created_at = Date.now(); // will need this for cache invalidation once database setup
+}
+
+// Routes
+app.get('/location', getLocation);
+
+// Route Handlers
+// getLocation function
+
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
