f0dee37850df4aab1563cc6688cc2352670594cd
Holly Davis <hollydavis541@gmail.com>
2019-12-07 14:41:36 -0800

    added weather lookup and weather callback

--- modules/locations.js
+++ modules/locations.js
@@ -1,19 +1,26 @@
+'use strict';
+
+// Dependencies
 require('dotenv').config();
 const superagent = require('superagent');
 const pg = require('pg');
 const Error = require('./error');
 
+// Setup
 const client = new pg.Client(process.env.DATABASE_URL);
 client.on('err', err => console.error(err));
 
+
+// Constructor
 function Location(query, city){
   this.search_query = query;
   this.formatted_query = city.formatted_address;
   this.latitude = city.geometry.location.lat;
   this.longitude = city.geometry.location.lng;
-  this.created_at = Date.now(); // will need this for cache invalidation once database setup
+  this.created_at = Date.now();
 }
 
+// Get location from API
 Location.fetchLocation = (request,response) => {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
   return superagent.get(url)
@@ -40,28 +47,23 @@ Location.prototype.save = function() {
 };
 
 // Check Database
-Location.lookup = (handler) => {
-  const SQL = `SELECT * FROM locations WHERE search_query=$1`;
+Location.lookup = handler => {
+  const SQL = `SELECT * FROM locations WHERE search_query=$1;`;
   const values = [handler.query];
   return client.query(SQL, values)
     .then( results => {
-      if (results.rowCount > 0 && (Date.now() - (results.rows[0].created_at)) < 300000){ // 300000 milliseconds is just an arbitrary number for testing at this point
-        console.log('Got data from DB');
+      if (results.rowCount > 0) {
+        console.log('Got location data from DB');
         handler.cacheHit(results);
-      } else if (results.rowCount > 0 && (Date.now() - (results.rows[0].created_at)) >= 300000) {
-        console.log('In DB, but too old, fetching new info from API');
-        // const sqlDelete = `DELETE FROM locations WHERE search_query='${results.rows[0].search_query}';`
-        // client.query(sqlDelete);
-        handler.cacheMiss();
       } else {
-        console.log('No data in DB, fetching...');
-        handler.cacheMiss();
+        console.log('No location data in DB, fetching...');
+        handler.cacheMiss(results);
       }
     })
     .catch(console.error);
 };
 
-Location.getLocation = (request,response) => {
+Location.getLocation = (request, response) => {
   const locationHandler = {
     query: request.query.data,
     cacheHit: (results) => {
--- modules/weather.js
+++ modules/weather.js
@@ -1,18 +1,24 @@
+'use strict';
+
+// Dependencies
 require('dotenv').config();
 const superagent = require('superagent');
 const pg = require('pg');
 const Error = require('./error');
 
+// Setup
 const client = new pg.Client(process.env.DATABASE_URL);
 client.on('err', err => console.error(err));
 
+// Constructor
 function Weather(day) {
   this.forecast = day.summary;
   this.time = new Date(day.time * 1000).toString().slice(0,15);
   this.created_at = Date.now();
 }
 
-Weather.getWeather = (request, response) => {
+// Get weather from API
+Weather.fetchWeather = (request, response) => {
   const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
   return superagent.get(url)
     .then( data => {
@@ -41,6 +47,45 @@ Weather.prototype.save = function(id){
   return client.query(SQL, values);
 };
 
+// Check Database
+Weather.lookup = (handler) => {
+  const SQL = 'SELECT * FROM weather WHERE location_id=$1;';
+  const values = [handler.query.id];
+  return client.query(SQL, values)
+    .then( results => {
+      if(results.rowCount > 0 && (Date.now() - (results.rows[0].created_at)) < 3600000){ // data under an hour
+        console.log('Got weather data from DB');
+        handler.cacheHit(results);
+      } else if (results.rowCount > 0 && (Date.now() - (results.rows[0].created_at)) >= 3600000) { // data an hour or older
+        console.log('Weather info too old, fetching new info from API');
+        const sqlDelete = `DELETE FROM weather WHERE location_id=${[handler.query.id]}`;
+        client.query(sqlDelete);
+        handler.cacheMiss();
+      } else {
+        console.log('No weather data in DB, fetching...');
+        handler.cacheMiss();
+      }
+    })
+    .catch(console.error);
+};
+
+// Route Callback
+Weather.getWeather = (request,response) => {
+  const weatherHandler = {
+    query: request.query.data,
+    cacheHit: (results) => {
+      response.send(results.rows[0]);
+    },
+    cacheMiss: () => {
+      Weather.fetchWeather(request, response)
+        .then( data => response.send(data));
+    }
+  };
+  Weather.lookup(weatherHandler);
+}
+
+// Connect to Database
 client.connect();
 
+// Export Module
 module.exports = Weather;
